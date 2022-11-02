761831f806d5cc724d76c5f2af83ff191fb16a25
deepapoojary <32684575+deepapoojary@users.noreply.github.com>
2022-11-02 10:50:00 +0530

    Phase 1 project java files

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.project.lockedme;
+
+public class Main {
+	
+	/*Enter your desired Directory path */
+	public static final String path = "C:\\Users\\selva\\Downloads"; 
+	
+	public static void main(String[] args) {
+		MenuOptions menu = new MenuOptions();
+		menu.introduction();
+		menu.mainMenu();
+	}
+	
+}
